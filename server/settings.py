c5e82b3eeb29315e87b3ac9830a1635648677a03
Konlan21 <joekonlan16@gmail.com>
2024-01-15 18:02:37 +0000

    create new superuser

--- server/settings.py
+++ server/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@-d6826=x1w1uqjh0pnh6z=qjzg+&07(xn(on6d2*!&@_jhdc+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
